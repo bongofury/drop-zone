43e8f093c15b21ba53c1c8410e5e0b91e1368e2b
bongofury <edotesi@gmail.com>
2022-05-10 15:39:20 +0200

    Emit upload id when upload ends

--- src/hooks/uploadQueue.js
+++ src/hooks/uploadQueue.js
@@ -42,7 +42,7 @@ export default function useUploadQueue({
       if (config.autoUpload) {
         processQueue();
       }
-      context.emit('uploaded', readonly(uploadedItems.map(({ file, response }) => ({ file, response }))));
+      context.emit('uploaded', readonly(uploadedItems.map(({ file, response, id }) => ({ file, response, id }))));
     };
     const onError = (ids, error) => {
       if (config.autoUpload) {
